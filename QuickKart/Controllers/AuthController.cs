a6ec0e7a533214355d83f29ac75b718d171ab042
Saurabh Kumar <saurabh.kumardev23@gmail.com>
2026-08-25 22:04:28 +0530

    initial commit

---
+++ QuickKart/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickKart.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Unauthorized();
+        }
+
+        [HttpPost("signup")]
+        public IActionResult SignUp()
+        {
+            return Unauthorized();
+        }
+    }
+}
